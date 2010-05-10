27a42f5f8ae468d0f23e3f709d8f682ecddb03cc
Carl Knutson <carl@infochimps.org>
2010-05-10 17:54:33 +0000

    Fixed end of day issues

--- bin/file_monitor.rb
+++ bin/file_monitor.rb
@@ -9,15 +9,18 @@ Settings.read 'graphite.yaml'
 Settings.define :workdir, :description => "Base directory where scrapers store files. (Ex: /data/ripd/com.tw)", :default => "/data/ripd/com.tw"
 Settings.resolve!
 
-WORK_DIR = '/data/ripd/com.tw/'
+# WORK_DIR = '/data/ripd/com.tw/'
 Log = Logger.new($stderr) unless defined?(Log)
 
 class FileMonitor
 
+  FileNameSize = Struct.new(:name, :size)
+
   def initialize
     return if handles.empty?
     @last_size = Hash.new
-    handles.each{|handle| @last_size[handle] = get_sizes(handle)[0] }
+    @current_file = Hash.new
+    handles.each{|handle| current_file(handle); @last_size[handle] = @current_file[handle].size }
   end
 
   def date_today     
@@ -25,19 +28,34 @@ class FileMonitor
   end
 
   def handles
-    @handles = `ls #{WORK_DIR}`.split("\n")
-    @handles.each{|handle| @handles.delete(handle) if !(`ls #{WORK_DIR + handle}`.split("\n").include?(date_today)) }
+    @handles = `ls #{Settings.workdir}`.split("\n")
+    new_handles = []
+    @handles.each{|handle| new_handles += [handle] if `ls #{Settings.workdir + "/" + handle}`.split("\n").include?(date_today) }
+    @handles = new_handles
     return @handles
   end
 
   def files handle
-    `ls -lt #{WORK_DIR + "/" + handle + "/" + date_today}`
+    `ls -lt #{Settings.workdir + "/" + handle + "/" + date_today}`
   end
 
   def get_sizes handle
     size_list = files(handle).scan(/^[\-dlrwx]{10}\s\d\s\w+\s\w+\s+(\d+)/).flatten
     return size_list if size_list.empty?
     size_list.map{|size| size.to_i}
+  end
+
+  def current_file handle
+    @current_file[handle] ||= FileNameSize["",nil]
+    file_list = files(handle).split("\n")
+    if file_list.empty?
+      @current_file[handle].size = `ls -l #{Settings.workdir + "/" + handle + "/" + @current_file[handle].name}`.scan(/^[\-dlrwx]{10}\s\d\s\w+\s\w+\s+(\d+)/).flatten[0]
+      return @current_file[handle] 
+    end
+    @current_file[handle].size, @current_file[handle].name = file_list[1].scan(/^[\-dlrwx]{10}\s\d\s\w+\s\w+\s+(\d+)\s\d{4}\-\d{2}\-\d{2}\s\d{2}\:\d{2}\s([^\s]+)/).flatten
+    @current_file[handle].name = date_today + "/" + @current_file[handle].name
+    @current_file[handle].size = @current_file[handle].size.to_i
+    return @current_file[handle]
   end
 
   def num_files handle
@@ -67,16 +85,15 @@ class FileMonitor
   end
 
   def size_rate handle
-    current_size = get_sizes(handle)[0]
-    if current_size.nil?
-      return current_size
+    if current_file(handle).size.nil?
+      return current_file(handle).size
     end
-    if current_size < @last_size[handle]
-      @last_size[handle] = current_size
-      return current_size
+    if current_file(handle).size < @last_size[handle]
+      @last_size[handle] = current_file(handle).size
+      return current_file(handle).size
     end
-    rate = current_size - @last_size[handle]
-    @last_size[handle] = current_size
+    rate = current_file(handle).size - @last_size[handle]
+    @last_size[handle] = current_file(handle).size
     return rate
   end    
 
@@ -92,7 +109,7 @@ class FileMonitor
           sizes = get_sizes(handle)
           @last_size[handle] = sizes[0] if @last_size[handle].nil?
           rate = size_rate(handle)
-          metrics << ["scraper.#{hostname}.com_tw.#{handle.chomp.gsub(".","_")}.current_file_size", sizes[0]] unless sizes.empty?
+          metrics << ["scraper.#{hostname}.com_tw.#{handle.chomp.gsub(".","_")}.current_file_size", current_file(handle).size] unless current_file(handle).size.nil?
           metrics << ["scraper.#{hostname}.com_tw.#{handle.chomp.gsub(".","_")}.size_rate", rate] unless rate.nil?
           metrics << ["scraper.#{hostname}.com_tw.#{handle.chomp.gsub(".","_")}.num_files", num_files(handle)]
           metrics << ["scraper.#{hostname}.com_tw.#{handle.chomp.gsub(".","_")}.avg_file_size", avg_size(handle)]
