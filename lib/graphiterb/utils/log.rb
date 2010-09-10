4b30ac917daea318d794d52e536b52c18cf33a60
Dhruv Bansal <dhruv@ph.utexas.edu>
2010-09-10 17:04:23 -0500

    Log will default to STDOUT.

--- lib/graphiterb/utils/log.rb
+++ lib/graphiterb/utils/log.rb
@@ -4,10 +4,8 @@ module Graphiterb
 
   class << self; attr_accessor :log end
 
-  # Create a Logger and point it at Graphiterb::LOG_FILE_DESTINATION which is
-  # set in ~/.imwrc and defaults to STDERR.
   def self.instantiate_logger!
-    Graphiterb.log ||= Logger.new(Settings[:log])
+    Graphiterb.log ||= Logger.new(Settings[:log] || STDOUT)
     Graphiterb.log.datetime_format = "%Y%m%d-%H:%M:%S "
     Graphiterb.log.level           = Logger::INFO
   end
